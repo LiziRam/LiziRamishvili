a0a5548e9566adc71489fac554ab3ce7b5213f8f
Lizi Ramishvili <liziramishvili@Lizis-MacBook-Air.local>
2022-10-30 23:20:05 +0400

    finish Day_4 tasks

---
+++ Day_04/Practical_4.2/Practical_4.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practical_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1; i <= input; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum from 1 to " + input + " is " + sum);
+        }
+    }
+}
+
---
+++ Day_04/Practical_4.4/Practical_4.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practical_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1; i <= input; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Sum of odd numbers from 1 to " + input + " is " + sum);
+        }
+    }
+}
+
