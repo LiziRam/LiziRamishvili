9483c71743accc3a3bd9591102ac939fd14d1136
Lizi Ramishvili <liziramishvili@Lizis-MacBook-Air.local>
2022-10-30 23:09:54 +0400

    finish Day_02 tasks

---
+++ Day_02/Practice_2.1/Practice_2.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Internal;
+
+namespace Practice_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //With third variable
+            Console.WriteLine("Swap values using third variable");
+            int A = 5;
+            int B = 7;
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            //Keep value of A in a dummy integer
+            int dummy = A;
+            //Swap values
+            A = B;
+            B = dummy;
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+
+            Console.WriteLine();
+
+            //Without third variable
+            Console.WriteLine("Swap values without using third variable");
+            int C = 6;
+            int D = 8;
+            Console.WriteLine(C);
+            Console.WriteLine(D);
+            C = C * D;
+            D = C / D;
+            C = C / D;
+
+            Console.WriteLine(C);
+            Console.WriteLine(D);
+        }
+    }
+}
+
