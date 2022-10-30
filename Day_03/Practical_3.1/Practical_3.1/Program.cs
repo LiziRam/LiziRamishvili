a40136c0ce42342919ba98628739a705ae3d748b
Lizi Ramishvili <liziramishvili@Lizis-MacBook-Air.local>
2022-10-30 23:14:16 +0400

    finish Day_3 tasks

---
+++ Day_03/Practical_3.1/Practical_3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practical_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter integer number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string oddOrEven;
+            if (number % 2 == 0)
+            {
+                oddOrEven = "even";
+            }
+            else
+            {
+                oddOrEven = "odd";
+            }
+            Console.WriteLine("Entered number " + number + " is " + oddOrEven);
+        }
+    }
+}
+
---
+++ Day_03/Practical_3.4/Practical_3.4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Practical_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birth year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int remainder = year % 12;
+            string chineseYear;
+            switch (remainder)
+            {
+                case 1:
+                    chineseYear = "Rooster";
+                    break;
+                case 2:
+                    chineseYear = "Dog";
+                    break;
+                case 3:
+                    chineseYear = "Pig";
+                    break;
+                case 4:
+                    chineseYear = "Rat";
+                    break;
+                case 5:
+                    chineseYear = "Ox";
+                    break;
+                case 6:
+                    chineseYear = "Tiger";
+                    break;
+                case 7:
+                    chineseYear = "Rabbit";
+                    break;
+                case 8:
+                    chineseYear = "Dragon";
+                    break;
+                case 9:
+                    chineseYear = "Snake";
+                    break;
+                case 10:
+                    chineseYear = "Horse";
+                    break;
+                case 11:
+                    chineseYear = "Sheep";
+                    break;
+
+                default:
+                    chineseYear = "Monkey";
+                    break;
+            }
+
+            Console.WriteLine(year + " was a " + chineseYear + " year");
+        }
+    }
+}
+
