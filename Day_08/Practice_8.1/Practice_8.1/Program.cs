62112bd8fa012e458f8ef78f521c2d5e954abcb0
Lizi Ramishvili <ramishvili.lizi@yahoo.com>
2022-11-06 00:39:21 +0400

    finish Day_08 tasks

---
+++ Day_08/Practice_8.1/Practice_8.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 13;
+            PrintRecursive(13, 1);
+        }
+
+        private static void PrintRecursive(int n, int curr)
+        {
+            if(curr > n)
+            {
+                return;
+            }
+
+            Console.Write(curr + " ");
+            PrintRecursive(n, curr + 1);
+        }
+    }
+}
+
---
+++ Day_08/Practice_8.4/Practice_8.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice_8._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            int degree = 10;
+            Console.WriteLine(PowerOfNum(number, degree, 1));
+        }
+
+        private static int PowerOfNum(int number, int degree, int res)
+        {
+            if(degree == 0)
+            {
+                return res;
+            }
+            return PowerOfNum(number, degree - 1, res * number);
+        }
+    }
+}
+
