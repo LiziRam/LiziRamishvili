579109fa67b8aa561ba760685f35b71e48c57599
Lizi Ramishvili <ramishvili.lizi@yahoo.com>
2022-12-03 17:58:59 +0400

    finish Mid_2 tasks

---
+++ Midterm_2/Mid_2.1/Mid_2.1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+
+namespace Mid_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/Users/liziramishvili/Desktop/LiziRamishvili/Midterm_2/Mid_2.1/Mid_2.1/Tests.txt";
+
+            Console.WriteLine("1. Start test \n2. Add test");
+            Console.Write("Choose 1 or 2: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            //If we choose to start the test
+            if(choice == 1)
+            {
+                StreamReader sr;
+                try
+                {
+                    sr = new StreamReader(path);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                
+                int correct = 0;
+                int guessed = 0;
+                int questions = 0;
+
+                string currLine = sr.ReadLine();
+                if(currLine == null)
+                {
+                    Console.WriteLine("There are no questions entered yet.");
+                }
+                else
+                {
+                    while (currLine != null)
+                    {
+                        questions += 1;
+                        string[] splitLine = currLine.Split("|");
+                        Console.WriteLine();
+                        Console.WriteLine($"Question: {splitLine[0]}");
+                        for (int i = 1; i <= 4; i++)
+                        {
+                            string alternative = splitLine[i];
+                            if (alternative[alternative.Length - 1] != '*')
+                            {
+                                Console.WriteLine($"\t{i}. {alternative}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"\t{i}. {alternative.Substring(0, alternative.Length - 1)}");
+                                correct = i;
+                            }
+                        }
+                        Console.Write("Enter 1, 2, 3 or 4 for an answer: ");
+                        int answer = Convert.ToInt32(Console.ReadLine());
+                        if (answer == correct)
+                        {
+                            guessed += 1;
+                        }
+                        currLine = sr.ReadLine();
+                    }
+                    Console.WriteLine($"Answered correctly: {guessed} / {questions}");
+                }
+                sr.Close();
+            }
+
+            //If we choose to add to the test
+            else if(choice == 2)
+            {
+                StreamWriter sw;
+                try
+                {
+                    sw = new StreamWriter(path, true);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                Console.WriteLine("First, write a question: ");
+                string question = Console.ReadLine();
+                while(question != "")
+                {
+                    string[] linesWithInfo = new string[5];
+                    linesWithInfo[0] = question;
+                    Console.WriteLine("Now enter 4 possible answers. Mark correct one with \'*\'");
+                    Console.WriteLine("(It should be the last charachter of the correct answer's string)");
+                    bool rightAnswerEntered = false;
+                    for (int i = 1; i <= 4; i++)
+                    {
+                        Console.WriteLine($"Answer {i}: ");
+                        linesWithInfo[i] = Console.ReadLine();
+                        int currLength = linesWithInfo[i].Length;
+                        if (linesWithInfo[i][currLength - 1] == '*')
+                        {
+                            rightAnswerEntered = true;
+                        }
+                    }
+                    if (!rightAnswerEntered)
+                    {
+                        throw new rightAnswerNotEnteredException();
+                    }
+                    sw.WriteLine(questionAndAnswers(linesWithInfo));
+                    Console.WriteLine("Write next question. If you want to finish the program, press ENTER: ");
+                    question = Console.ReadLine();
+                }
+                sw.Close();
+            }
+        }
+
+        public static string questionAndAnswers(string[] info)
+        {
+            string res = "";
+            for(int i = 0; i < info.Length; i++)
+            {
+                res += info[i] + "|";
+            }
+            return res;
+        }
+    }
+}
+
---
+++ Midterm_2/Mid_2.2/Mid_2.2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mid_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/Users/liziramishvili/Desktop/LiziRamishvili/Midterm_2/Mid_2.2/Mid_2.2/Words.txt";
+            List<string> words = new List<string>();
+            StreamReader sr;
+            try
+            {
+                sr = new StreamReader(path);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            string currWord = sr.ReadLine();
+            while(currWord != null)
+            {
+                words.Add(currWord);
+                currWord = sr.ReadLine();
+            }
+            sr.Close();
+
+            WordLibrary lib = new WordLibrary(words);
+            Random r = new Random();
+            int maxWords = lib.WordCount;
+            string randomWord = lib[r.Next(0, maxWords)];
+            char[] hidden = new char[randomWord.Length];
+            for(int i = 0; i < randomWord.Length; i++)
+            {
+                hidden[i] = '_';
+            }
+
+            Console.WriteLine("Try to guess the word: ");
+            Console.WriteLine(updatedWord(hidden));
+            //Console.WriteLine(randomWord); //To check
+
+            int mistakes = 0;
+            bool gameLost = false;
+            bool gameWon = false;
+
+            while(!gameLost && !gameWon)
+            {
+                Console.Write("Enter a letter: ");
+                char c = Convert.ToChar(Console.ReadLine());
+                bool guessed = false;
+                for(int i = 0; i < randomWord.Length; i++)
+                {
+                    if(c == randomWord[i] || char.ToUpper(c) == randomWord[i])
+                    {
+                        hidden[i] = c;
+                        guessed = true;
+                    }
+                }
+                if (guessed)
+                {
+                    Console.WriteLine($"Nice job, This is how the word looks like now: {updatedWord(hidden)}");
+                }
+                else
+                {
+                    mistakes += 1;
+                    Console.WriteLine($"Wrong, you lost a {(BodyPartsEnum)mistakes}.");
+                }
+
+                if(mistakes == 6)
+                {
+                    gameLost = true;
+                    Console.WriteLine("Game over, you looser.");
+                }
+                if (foundWord(hidden))
+                {
+                    gameWon = true;
+                    Console.WriteLine("You Won, Nice !");
+                }
+            }
+        }
+
+        public static bool foundWord(char[] arr)
+        {
+            bool res = true;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '_')
+                {
+                    res = false;
+                }
+            }
+            return res;
+        }
+
+        //ამის გარეშე დეშებს შორის დაშორებები არ ჩანდა 
+        public static string updatedWord(char[] arr)
+        {
+            string res = "";
+            for(int i = 0; i < arr.Length; i++)
+            {
+                res += arr[i] + " ";
+            }
+            return res;
+        }
+    }
+}
+
