7f02b2c9410a4be4b40b69fe130a1163d5671c85
Lizi Ramishvili <ramishvili.lizi@yahoo.com>
2022-12-18 00:18:39 +0400

    Finish Day_24 tasks

---
+++ Day_24/Practice_24.1/Practice_24.1/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Practice_24._1
+{
+	public class Customer
+	{
+		private int _customerId;
+		private string _customerName;
+
+		public Customer(int id, string name)
+		{
+			_customerId = id;
+			_customerName = name;
+		}
+
+		public int CustomerId { get { return _customerId; }  }
+        public string CustomerName { get { return _customerName; } }
+    }
+}
+
