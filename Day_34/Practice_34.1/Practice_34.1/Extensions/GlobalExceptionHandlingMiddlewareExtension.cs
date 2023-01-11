594985a1aa15d4150b459bf58a20b7b032e7e1f1
Lizi Ramishvili <ramishvili.lizi@yahoo.com>
2023-01-11 23:57:34 +0400

    Finish Day_34 tasks

---
+++ Day_34/Practice_34.1/Practice_34.1/Extensions/GlobalExceptionHandlingMiddlewareExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Practice_34._1.Middlewares;
+
+namespace Practice_34._1.Extensions
+{
+	public static class GlobalExceptionHandlingMiddlewareExtension
+	{
+		public static IApplicationBuilder UseGlobalExceptionHandling(this IApplicationBuilder builder)
+		{
+			builder.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+			return builder;
+		}
+	}
+}
+
